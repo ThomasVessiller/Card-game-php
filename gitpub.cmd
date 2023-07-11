5a5d296ec652a9e410d0910d90fca68ce817c509
thomas <thomas.vessiller.tv@gmail.com>
2023-07-11 18:54:11 +0200

    auto commit

--- gitpub.cmd
+++
@@ -1,4 +0,0 @@
-
-git add .
-git commit -m "auto commit"
-git push --set-upstream card-game master
--- projet php/setup.cmd
+++
@@ -1,6 +0,0 @@
-
-cd C:\Users\thoma\Desktop\php-cours\projet php
-docker compose up -d
-
-start http://localhost:8080/
-start http://localhost
